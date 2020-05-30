c519a263922dc4e7f88cd90338dd4700b1676134
Tobias Schlick <Tobias.Schlick@gmx.de>
2020-05-30 16:21:32 +0200

    stats

--- helper/Stats.py
+++ helper/Stats.py
@@ -1,14 +1,22 @@
 import helper
+from pathlib import Path
+import csv
 
 Terminal = helper.Terminal()
 
 
 class Stats:
     def __init__(self):
-        self.filepath = f"05_Statistik"
+        self.filepath = f"./Excelfiles/05_Statistik.csv"
 
     def write_Groups(self, bezirk, counter):
-        Terminal.print(f"{bezirk} - {counter}")
+        if self.check_file():
+            Terminal.print(f"File {self.filepath} does already exist")
+        else:
+            with open(self.filepath, "a", newline="") as csvfile:
+                writer = csv.writer(csvfile, delimiter=';', quotechar='|')
+                Terminal.print(f"File {self.filepath} created")
+                writer.writerow([bezirk, counter])
 
     def write_Teams(self):
         pass
@@ -18,3 +26,10 @@ class Stats:
 
     def write_Mails(self):
         pass
+
+    def check_file(self):
+        checkfile = Path(self.filepath)
+        if checkfile.is_file():
+            return True
+        else:
+            return False
